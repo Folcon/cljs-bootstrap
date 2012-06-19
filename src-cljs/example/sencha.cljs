4672ab4d42bcf855aa045809102b4c654e22f030
Dustin Getz <dustin.getz@gmail.com>
2012-06-19 15:27:42 -0400

    whacked dead code in sencha example

--- src-cljs/example/sencha.cljs
+++ src-cljs/example/sencha.cljs
@@ -1,30 +1,12 @@
-;; (defn ^:export init []
-;;   (Ext.Application.
-;;    (make-js-map
-;;     {"launch" #(Ext.Panel.
-;;                 (make-js-map {"fullscreen" true,
-;;                               "html" "Hello World!!"}))})))
-
-;;Ext.Msg.alert ('Status', 'Changes saved successfully.');
-;(Ext.Msg.alert "status" "helloworld.")
-;(Ext.Msg.show (make-js-map {"title" "hello" "msg" "world" }))
-
 (ns example.sencha)
-
-(defn jsobj
-  "makes a javascript map from a clojure one
-   (stolen from https://gist.github.com/1098417) "
-  [cljmap]
-  (let [out (js-obj)]
-    (doall (map #(aset out (name (first %)) (second %)) cljmap))
-    out))
 
 (defn clj->js
   "Recursively transforms ClojureScript maps into Javascript objects,
    other ClojureScript colls into JavaScript arrays, and ClojureScript
    keywords into JavaScript strings.
 
-   Borrowed and updated from mmcgrana."
+   Borrowed and updated from mmcgrana.
+  http://techylinguist.com/posts/2012/01/23/clojurescript-getting-started/"
   [x]
   (cond
     (string? x) x
@@ -42,8 +24,8 @@
 
 (defn launch []
   (.create js/Ext "Ext.container.Viewport" (clj->js
-                                            {"layout" "border"
-                                             "items" [panel]})))
+                                            {:layout "border"
+                                             :items [panel]})))
 
 (defn ^:export bootstrap []
   (.application js/Ext (clj->js
